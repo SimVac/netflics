7ba4117ff560cebff9d3d36967ce55a8bb756267
Andrea Scarsato <106865296+ScarsatoAndrea@users.noreply.github.com>
2024-01-13 09:49:38 +0000

    non risolve nessun problema

--- movie/src/routes/+page.svelte
+++ movie/src/routes/+page.svelte
@@ -1,14 +1,39 @@
 <script context="module">
-        const url = 'https://api.themoviedb.org/3/movie/popular?api_key=&language=en-US&page=1';
+	
+	export async function load ({fetch}){
+		console.log("ciao")
+		const res = await fetch (
+			`https://api.themoviedb.org/3/movie/popular?api_key=80390a3069aa4ba033c8010d672709bc&language=en-US&page=1`
+		);
+		const data = await res.json();
+		console.log(data)
+		if (res.ok){
+			return{
+				props : {popular: data.results}
+			}
+		}
+	}
+	load();
+	
+	/*const url =
+		'https://api.themoviedb.org/3/movie/popular?api_key=80390a3069aa4ba033c8010d672709bc&language=en-US&page=1';
 
-        fetch(url)
-        .then((res) => res.json())
-        .then((json) => console.log(json))
-        .catch(err => console.error('error:' + err));
-
+	fetch(url)
+		.then((res) => res.json())
+		.then((json) => {
+			console.log(json);
+			porps: {popular : json};
+		})
+		.catch((err) => console.error('error:' + err));*/
 
 </script>
 
 <script>
-
+	import PopularMovies from './components/PopularMovies.svelte';
+	export let popular = [];
+	console.log(popular)
 </script>
+
+<section>
+	<PopularMovies movies={popular} />
+</section>
---
+++ movie/src/routes/components/PopularMovies.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let movies;
+    console.log(movies[0])
+</script>
+
+<h3>Popular Movies: </h3>
+
+<div class="popular-movies">
+    <div class="movie">
+
+    </div>
+</div>
