68a496a617f3eacece738e86d877f1cbcf537ce4
SimVac <103424187+SimVac@users.noreply.github.com>
2024-01-23 08:46:08 +0000

    Home

--- movie/src/lib/components/Nav.svelte
+++ movie/src/lib/components/Nav.svelte
@@ -13,12 +13,17 @@
         align-items: center;
         justify-content: space-around;
         width: 20vw;
+        background: rgba(255, 255, 255, 0.07);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(2.4px);
+        -webkit-backdrop-filter: blur(2.4px);
     }
     a{
         font-size: 1rem;
         font-weight: bold;
         font-family: 'Poppins';
-        color: black;
+        color: rgb(248, 11, 11);
         text-decoration: none;
     }
     .cont{
--- movie/src/routes/+page.svelte
+++ movie/src/routes/+page.svelte
@@ -1,5 +1,91 @@
-<h1>Netflics</h1>
-<button><a href="/explore">Esplora</a></button>
-<button><a href="/explore">Watchlist</a></button>
+<script>
+    import { onMount } from 'svelte';
+ 
+    onMount(() => {
+        let button = document.querySelector('.button');
+        let testA = document.querySelector('a');
+        let backgroundButton = document.querySelector('.background__button');
+
+        button.addEventListener('mouseenter', function(){
+            testA.classList.add('is-white')
+            backgroundButton.classList.add('is-hover');
+        });
+
+        button.addEventListener('mouseleave', function(){
+            testA.classList.remove('is-white')
+            backgroundButton.classList.remove('is-hover');
+        });
+    });
+</script>
+
+<section>
+    <h1 class="centered">NETFLICS</h1>
+    <div class="centered">
+        <div id="buttons">
+            <a href="/explore"><button class="button">EXPLORE</button></a>
+            <a href="/explore"><button class="button">WATCHLIST</button></a>
+        </div>
+    </div>
+</section>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    
+    :global(body) {
+        background-image: url("home_wallpaper.jpg");
+    }
+
+    h1 {
+        font-size: 13vw;
+        color: rgb(248, 11, 11);
+    }
+
+    .centered {
+        display: flex;
+        justify-content: center;
+    }
+
+    #buttons {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 40%;
+    }
+
+    .button {
+        width: 10vw;
+        height: 8vh;
+        background-color: rgb(248, 11, 11);
+        border: none;
+        border-radius: 10px;
+        font-family: 'Roboto';
+        color: #fffafa;
+        font-weight: 500;
+        letter-spacing: 1px;
+        font-size: 1vw;
+    }
+
+    .button:hover {
+        color: red;
+        background-color: #fffafa;
+        cursor: pointer;
+    }
+    
+    a:link { 
+        text-decoration: none; 
+    } 
+
+    a:visited { 
+        text-decoration: none; 
+    } 
+
+    a:hover { 
+        text-decoration: none; 
+    } 
+
+    a:active { 
+        text-decoration: none; 
+    }
+</style>
 
 
