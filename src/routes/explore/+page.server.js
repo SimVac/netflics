8f9050faa338d96a56a91facb926d222a6f6017c
Mattia Corradini <106865419+MattiaCorradini@users.noreply.github.com>
2024-01-26 11:52:21 +0000

    aaaaa

--- src/routes/explore/+page.server.js
+++ src/routes/explore/+page.server.js
@@ -1,11 +1,11 @@
 import { API_KEY } from '$lib/env';
 
-export async function load({ url }) {
-    const popular = await get_data('/api/popular');
+export async function load({ fetch, url }) {
+    const popular = await get_data(fetch, '/api/popular');
     return { popular };
 }
 
-async function get_data (url) {
+async function get_data (fetch, url) {
     const res = await fetch(url);
     const data = await res.json();
     return data.results;
