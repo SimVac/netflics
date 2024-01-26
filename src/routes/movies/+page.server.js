e466d6a32ad0a5c8b9bb0fa57d3de6227a1b9d86
Mattia Corradini <106865419+MattiaCorradini@users.noreply.github.com>
2024-01-26 11:54:24 +0000

    asdasd

--- src/routes/movies/+page.server.js
+++ src/routes/movies/+page.server.js
@@ -1,11 +1,11 @@
-const get_details = (async(id) => {
+const get_details = (async(fetch, id) => {
     const res = await fetch('/api/movie?' + new URLSearchParams({id}));
     const movieDetails = await res.json();
     return { movieDetails };
 })
 
-export async function load({ url }) {
+export async function load({ fetch, url }) {
     let id = url.searchParams.get('id');
-    let details = await get_details(id);
+    let details = await get_details(fetch, id);
     return {details};
 }
