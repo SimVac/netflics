d0acb2b78f18c8c33f9a79d1b3f05bb96f361139
SimVac <103424187+SimVac@users.noreply.github.com>
2024-01-27 10:07:17 +0100

    Update +page.server.js

--- src/routes/search/+page.server.js
+++ src/routes/search/+page.server.js
@@ -1,13 +1,13 @@
-const get_details = (async(searched) => {
+const get_details = (async(fetch, searched) => {
     const res = await fetch('/api/search?' + new URLSearchParams({searched}));
     const movieDetails = await res.json();
     return { movieDetails };
 })
 
-export async function load({ url }) {
+export async function load({ fetch, url }) {
     let searched = url.searchParams.get('q');
     console.log(searched);
-    let searchedMovie = await get_details(searched);
+    let searchedMovie = await get_details(fetch, searched);
     console.log(searchedMovie);
     return {searchedMovie};
 }
